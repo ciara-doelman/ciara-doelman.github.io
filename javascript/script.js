e7dbdfac6986caba0206d58df1cf782e4af4d867
Ciara Doelman <cbdoelman@gmail.com>
2022-09-19 17:15:49 +1200

    completed neuroplasticity blog

--- javascript/script.js
+++ javascript/script.js
@@ -71,3 +71,75 @@ parentContainer4.addEventListener('click', (event) => {
     ? 'Read Less...'
     : 'Read More...'
 })
+
+const parentContainer5 = document.querySelector('.read-more-container5')
+
+parentContainer5.addEventListener('click', (event) => {
+  const current = event.target
+
+  const isButton = current.className.includes('button')
+
+  console.log('read more button pressed')
+  if (!isButton) return
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Read More')
+    ? 'Read Less...'
+    : 'Read More...'
+})
+
+const parentContainer6 = document.querySelector('.read-more-container6')
+
+parentContainer6.addEventListener('click', (event) => {
+  const current = event.target
+
+  const isButton = current.className.includes('button')
+
+  console.log('read more button pressed')
+  if (!isButton) return
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Read More')
+    ? 'Read Less...'
+    : 'Read More...'
+})
+
+const parentContainer7 = document.querySelector('.read-more-container7')
+
+parentContainer7.addEventListener('click', (event) => {
+  const current = event.target
+
+  const isButton = current.className.includes('button')
+
+  console.log('read more button pressed')
+  if (!isButton) return
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Read More')
+    ? 'Read Less...'
+    : 'Read More...'
+})
+
+const parentContainer8 = document.querySelector('.read-more-container8')
+
+parentContainer8.addEventListener('click', (event) => {
+  const current = event.target
+
+  const isButton = current.className.includes('button')
+
+  console.log('read more button pressed')
+  if (!isButton) return
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Read More')
+    ? 'Read Less...'
+    : 'Read More...'
+})
