1917f893aad1f64fc0a142d3461b0accb6b8d32d
Ciara Doelman <cbdoelman@gmail.com>
2022-09-19 15:00:27 +1200

    improved read more buttons and started neuroplasticity blog

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,73 @@
-function learnLoops() {
+// READ MORE BUTTONS
 
-  if
+const parentContainer = document.querySelector('.read-more-container')
 
-}
+parentContainer.addEventListener('click', (event) => {
+  const current = event.target
 
+  const isButton = current.className.includes('button')
 
+  console.log('read more button pressed')
+  if (!isButton) return
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Read More')
+    ? 'Read Less...'
+    : 'Read More...'
+})
+
+const parentContainer2 = document.querySelector('.read-more-container2')
+
+parentContainer2.addEventListener('click', (event) => {
+  const current = event.target
+
+  const isButton = current.className.includes('button')
+
+  console.log('read more button pressed')
+  if (!isButton) return
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Read More')
+    ? 'Read Less...'
+    : 'Read More...'
+})
+
+const parentContainer3 = document.querySelector('.read-more-container3')
+
+parentContainer3.addEventListener('click', (event) => {
+  const current = event.target
+
+  const isButton = current.className.includes('button')
+
+  console.log('read more button pressed')
+  if (!isButton) return
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Read More')
+    ? 'Read Less...'
+    : 'Read More...'
+})
+
+const parentContainer4 = document.querySelector('.read-more-container4')
+
+parentContainer4.addEventListener('click', (event) => {
+  const current = event.target
+
+  const isButton = current.className.includes('button')
+
+  console.log('read more button pressed')
+  if (!isButton) return
+  const currentText = event.target.parentNode.querySelector('.read-more-text')
+
+  currentText.classList.toggle('read-more-text--show')
+
+  current.textContent = current.textContent.includes('Read More')
+    ? 'Read Less...'
+    : 'Read More...'
+})
