8d4ce87977635f29c8136cdee222034565acf277
Ciara Doelman <cbdoelman@gmail.com>
2022-10-07 09:16:43 +1300

    added background to purple generative art

--- art/purple.js
+++ art/purple.js
@@ -1,11 +1,12 @@
 function setup() {
   createCanvas(windowWidth, windowHeight)
   noStroke()
+  background(255, 100, 255, 150)
 }
 
 function draw() {
   if (mouseIsPressed) {
-    fill(random(255), random(100), random(255), random(255))
+    fill(random(255), random(50), random(210), random(255))
   } else {
     noFill()
   }
